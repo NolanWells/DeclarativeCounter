5158493deded12d09f53edde592c0066db3f0a3f
unknown <nolan@thewellsfamily.us>
2023-04-04 21:12:57 -0700

    finished

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import './App.css';
+
+import React, { useState } from 'react'
+
+function App() {
+  let [counterNum, setCounterNum ] = useState(0)
+  window.setCounterNum = setCounterNum
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          {counterNum}
+        </p>
+       
+      </header>
+    </div>
+  );
+}
+
+export default App;
